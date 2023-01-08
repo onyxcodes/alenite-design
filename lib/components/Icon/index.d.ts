b508812942708e7775c7247dfaa2f440d9842eae
Onyx Ganda <onyxganda@gmail.com>
2023-01-08 07:15:04 -0500

    Remove tracking for libs

--- lib/components/Icon/index.d.ts
+++
@@ -1,6 +0,0 @@
-import 'styles/icons.scss';
-export interface IconProps {
-    name: string;
-}
-declare const Icon: (props: IconProps) => JSX.Element;
-export default Icon;
--- lib/hooks/useElementWidth/index.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- *
- * https://dev.to/vitaliemaldur/resize-event-listener-using-react-hooks-1k0c
- */
-declare const useElementWidth: (element?: HTMLElement | null, property?: 'clientWidth' | 'offsetWidth') => number;
-export default useElementWidth;
